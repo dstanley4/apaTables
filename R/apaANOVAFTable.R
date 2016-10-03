700b26d800e9057729433f6496a7aa2a7c853048
David Stanley <dstanley@uoguelph.ca>
2016-10-03 09:37:58 -0400

    Anova command updates

--- R/apaANOVAFTable.R
+++ R/apaANOVAFTable.R
@@ -13,21 +13,21 @@
 #' #Example 1: 1-way from Field et al. (2012) Discovery Statistics Using R
 #' options(contrasts = c("contr.sum", "contr.poly"))
 #' lm_output <- lm(libido ~ dose, data=viagra)
-#' apa.anova.table(lm_output,filename="ex1_anova_table.doc")
+#' apa.aov.table(lm_output,filename="ex1_anova_table.doc")
 #'
 #' # Example 2: 2-way from Fidler & Thompson (2001)
 #' # You must set these contrasts to ensure values match SPSS
 #' options(contrasts = c("contr.sum", "contr.poly"))
 #' lm_output <- lm(dv ~ a*b, data=fidler_thompson)
-#' apa.anova.table(lm_output,filename="ex2_anova_table.doc")
+#' apa.aov.table(lm_output,filename="ex2_anova_table.doc")
 #'
 #' #Example 3: 2-way from Field et al. (2012) Discovery Statistics Using R
 #' # You must set these contrasts to ensure values match SPSS
 #' options(contrasts = c("contr.sum", "contr.poly"))
 #' lm_output <- lm(attractiveness ~ gender*alcohol, data=goggles)
-#' apa.anova.table(lm_output,filename="ex3_anova_table.doc")
+#' apa.aov.table(lm_output,filename="ex3_anova_table.doc")
 #' @export
-apa.anova.table<-function(lm_output,filename,table.number=NA, conf.level=.90,type=3) {
+apa.aov.table<-function(lm_output,filename,table.number=NA, conf.level=.90,type=3) {
      table_number <- table.number
      conf_level <- conf.level
 
