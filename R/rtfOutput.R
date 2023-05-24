144d21f049d256044b3de6030d6b3595c30906db
David Stanley <dstanley@uoguelph.ca>
2023-05-23 21:33:27 -0400

    update

--- R/rtfOutput.R
+++ R/rtfOutput.R
@@ -6,14 +6,13 @@ apa.save <- function(filename, ...) {
      }
 }
 
-write.rtf.document <- function(filename, paper="us", list_of_tables) {
+write.rtf.document <- function(filename, list_of_tables, paper="us") {
      #generate document format code if needed
      doc.type <- list()
      doc.type$uslandscape <- "\\paperw15840 \\paperh12240 \\margl1440 \\margr1440 \\margt1440 \\margb1440 \\landscape "
      doc.type$usportrait <- ""
      doc.type$a4landscape <- "\\paperw16834 \\paperh11909 \\margl1440 \\margr1440 \\margt1440 \\margb1440 \\landscape "
      doc.type$a4portrait <- ""
-
 
      if (!any(paper == c("us","a4"))) {
           paper <- "us"
@@ -33,21 +32,20 @@ write.rtf.document <- function(filename, paper="us", list_of_tables) {
      txt.start <- "{\\rtf1\\ansi\\deff0 {\\fonttbl {\\f0 Times New Roman;}}"
      txt.end <- "}"
 
-
      # KEY PART HERE
      txt.body <- convert_tables_to_rtf_txt(list_of_tables, paper)
-#
-#      file.id <- file(filename,"wt")
-#      writeLines(txt.start,file.id)
+
+     file.id <- file(filename,"wt")
+     writeLines(txt.start,file.id)
 #      if (landscape==TRUE) {
 #           writeLines(txt.format,file.id)
 #      }
-#      length.body <- length(txt.body)
-#      for (i in 1:length.body) {
-#           writeLines(txt.body[i],file.id)
-#      }
-#      writeLines(txt.end,file.id)
-#      close(file.id)
+     length.body <- length(txt.body)
+     for (i in 1:length.body) {
+          writeLines(txt.body[i],file.id)
+     }
+     writeLines(txt.end,file.id)
+     close(file.id)
 }
 
 
@@ -136,36 +134,33 @@ get_end_page_txt <- function(list_of_tables, cur_table_number) {
 
 
 convert_single_table_to_txt <- function(cur_table_object){
-     # table.number.str <- ""
-     # if (is.na(table.number)) {
-     #      table.number.str <- "XX"
-     # } else {
-     #      table.number <- round(table.number)
-     #      table.number.str <- sprintf("%1.0f",table.number)
-     # }
-     #
-     #
-     # #document format
-     # doc.spec <- paste(paper,orientation,sep="")
-     # txt.format <- doc.type[[doc.spec]]
-     # txt.start <- "{\\rtf1\\ansi\\deff0 {\\fonttbl {\\f0 Times New Roman;}}"
-     # txt.end <- "}"
+
+     txt.body <- cur_table_object$txt.body
+
+     table.number.str <- ""
+     if (is.na(cur_table_object$table.number)) {
+          table.number.str <- "XX"
+     } else {
+          table.number <- round(table.number)
+          table.number.str <- sprintf("%1.0f",table.number)
+     }
 
      #Table X, title, and note
-     # blank.line <- c("{\\pard  \\par}")
-     # number.line <-sprintf("{\\pard Table %s \\par}",table.number.str)
-     # if (is.na(table.title)) {
-     #      title.line <- sprintf("{\\pard\\i Table title goes here \\par}")
-     # } else {
-     #      title.line <- sprintf("{\\pard\\i %s\\par}",table.title)
-     # }
-     # if (is.na(table.note)) {
-     #      note.line <- sprintf("{\\i Table note goes here}")
-     # } else {
-     #      note.line <- sprintf("{\\pard \\par}{\\pard{\\i Note.} %s\\par}",table.note)
-     # }
-     # txt.body <- c(number.line,blank.line,title.line, blank.line,txt.body,note.line)
+     blank.line <- c("{\\pard  \\par}")
+     number.line <-sprintf("{\\pard Table %s \\par}",table.number.str)
+     if (is.na(cur_table_object$table.title)) {
+          title.line <- sprintf("{\\pard\\i Table title goes here \\par}")
+     } else {
+          title.line <- sprintf("{\\pard\\i %s\\par}",cur_table_object$table.title)
+     }
+     if (is.na(cur_table_object$table.note)) {
+          note.line <- sprintf("{\\i Table note goes here}")
+     } else {
+          note.line <- sprintf("{\\pard \\par}{\\pard{\\i Note.} %s\\par}",cur_table_object$table.note)
+     }
+     txt.body <- c(number.line,blank.line,title.line, blank.line,txt.body,note.line)
 
+     return(txt.body)
 }
 
 
