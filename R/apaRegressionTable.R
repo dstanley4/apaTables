9161891fb56ff1720d43602781730a1760e647f8
cschwarz007 <christian_schwarz@gmx.de>
2025-03-20 20:25:05 +0100

    Update apaRegressionTable.R

--- R/apaRegressionTable.R
+++ R/apaRegressionTable.R
@@ -332,7 +332,10 @@ apa.reg.table<-function(...,filename=NA,table.number=0, prop.var.conf.level = .9
 
 apa_single_block<-function(cur_blk,is_random_predictors, prop_var_conf_level) {
      #Regression data
-     reg_table_data <- cur_blk$model
+     reg_table_data <- as.data.frame(model.matrix(cur_blk))
+     reg_table_data[,1] <- cur_blk$model[,1]
+     
+     #reg_table_data <- cur_blk$model
      n              <- dim(reg_table_data)[1]
      col_names      <- names(reg_table_data)
 
